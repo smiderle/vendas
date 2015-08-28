ff3da6be2eec504d9a4b5650b6f09931f299cecd
Ladair Smiderle <ladairsmiderle@gmail.com>
2015-08-27 21:32:21 -0300

    Ajustes.

--- vendas-parent/vendas-api/src/main/java/br/com/vendas/api/rest/v1/common/EmailController.java
+++ vendas-parent/vendas-api/src/main/java/br/com/vendas/api/rest/v1/common/EmailController.java
@@ -1,18 +1,32 @@
 package br.com.vendas.api.rest.v1.common;
 
 import javax.inject.Inject;
+import javax.ws.rs.core.Response;
 
+import br.com.vendas.domain.customer.Customer;
 import br.com.vendas.services.EmailBean;
+import br.com.vendas.services.support.ServiceResponse;
+import br.com.vendas.support.ApiResponse;
+import br.com.vendas.support.ResponseBuilder;
+import br.com.vendas.support.VendasExceptionWapper;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @RequestMapping(value="/public/v1/email")
 @Controller
 @EnableAsync
 public class EmailController {
+
+
+    private static final Logger LOG = Logger.getLogger( EmailController.class );
 
 
 	@Autowired
@@ -25,5 +39,37 @@ public class EmailController {
 		System.out.println("Concluido ");
 
 	}
+
+    @RequestMapping(value="contact", method = RequestMethod.GET)
+	public void contact( String name, String email, String message) {
+
+        String mensagem = "Nome do contato: " + name +"\n";
+        mensagem += "Email de contato: "+ email + "\n";
+        mensagem += "Mensagem: "+ message;
+
+        emailBean.sendEmail("ladairsmiderle@gmail.com", "ladairsmiderle@gmail.com", "Contato VendasUP", mensagem);
+
+    }
+
+
+    @RequestMapping(value = "contactPost", method = RequestMethod.POST)
+    public @ResponseBody ApiResponse contactPost(String name, String email, String message) {
+
+        try {
+
+            String mensagem = "Nome do contato: " + name + "\n";
+            mensagem += "Email de contato: " + email + "\n";
+            mensagem += "Mensagem: " + message;
+
+            emailBean.sendEmail("ladairsmiderle@gmail.com", "ladairsmiderle@gmail.com", "Contato VendasUP", mensagem);
+
+            return ResponseBuilder.build(new Integer(1));
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+            return ResponseBuilder.build(new VendasExceptionWapper(e));
+        }
+
+
+    }
 
 }
